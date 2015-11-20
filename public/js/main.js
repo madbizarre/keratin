f3c37ef8a509572c49848405ddfea2a69a2bb51f
madbizarre <andrew-cool@bk.ru>
2015-11-20 20:30:27 +0300

    add angular add list add detail

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,45 @@
-;(function(){
+;(function (){
+    angular.module('keratin', ['ngRoute', 'ngResource'])
 
-});
+        .config([
+            '$routeProvider', '$locationProvider',
+            function($routeProvide, $locationProvider){
+                $routeProvide
+                    .when('/',{
+                        templateUrl:'template/home.html',
+                        controller: ''
+                    })
+                    .when('/product/:productId', {
+                        templateUrl:'template/detail.html',
+                        controller:'ProductDetailController'
+                    })
+                    .otherwise({
+                        redirectTo: '/'
+                    });
+            }
+        ])
+
+        .controller('ProductListController',[ '$scope', '$http', function ($scope, $http){
+            $http.get('/products.json').success(function(data){
+                $scope.products = data;
+            }).error(function(data){
+
+            });
+
+        }])
+
+        .controller('ProductDetailController',[
+            '$scope','$http', '$routeParams',
+            function($scope, $http, $routeParams) {
+                $scope.productId = $routeParams.productId;
+
+                $http.get('/products.json').success(function(data){
+                    $scope.product = data[$scope.productId];
+                }).error(function(data){
+
+                });
+            }
+        ])
+
+    ;
+})();
--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,28 @@ var transporter = require('../lib/nodemailer');
 var config = require('../config');
 
 router.get('/', function (req, res, next){
-  res.render('index');
+    res.render('index');
 });
 
 router.post('/mail', function (req, res, next){
-  let mail = config.get('mail'),
-      text = `Имя: ${req.body.name || 'не указано'}
+    let mail = config.get('mail'),
+        text = `Имя: ${req.body.name || 'не указано'}
 Email: ${req.body.email || 'не указан'}
 Телефон: ${req.body.tel}
 Комментарий: ${req.body.comment || 'не указан'}`;
-  transporter.sendMail({
-    from: `${mail.from} <${mail.user}>`,
-    to: mail.to.split(', '),
-    subject: `Заявка с сайта: joindarkside.ru`,
-    text: text
-  }, function (err){
-    if (err) return next(err);
-    res.status(200).end();
-  });
+    transporter.sendMail({
+        from: `${mail.from} <${mail.user}>`,
+        to: mail.to.split(', '),
+        subject: `Заявка с сайта: joindarkside.ru`,
+        text: text
+    }, function (err){
+        if (err) return next(err);
+        res.status(200).end();
+    });
 });
 
 /*router.get('/!*', function(req, res){
-  res.redirect('/');
-});*/
+ res.redirect('/');
+ });*/
 
 module.exports = router;
